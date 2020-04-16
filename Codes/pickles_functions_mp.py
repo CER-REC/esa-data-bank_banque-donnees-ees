54bca02d4ab15f2e031ec69a4b0805e6b122b0a2
Vibudh Singh <Vibudh.Singh@cer-rec.gc.ca>
2020-04-16 17:34:04 -0600

    trial run for 10 files

--- Codes/pickles_functions_mp.py
+++ Codes/pickles_functions_mp.py
@@ -20,7 +20,7 @@ def pickle_pdf_xml(arguments):
     file = arguments[0]
     path = arguments[1]
     xml = parser.from_file(file, xmlContent = True)
-    replace_string = file.split('/')[-1].replace('.pdf', '')
+    replace_string = file.split('\\')[-1].replace('.pdf', '')
     save_string = path + replace_string + '.pkl'
     print(save_string)
     pickle.dump(xml, open(save_string, "wb"))
