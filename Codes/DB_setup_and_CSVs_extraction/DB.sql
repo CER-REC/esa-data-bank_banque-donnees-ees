de69eee6e1a3f0061a35b91ee38ee555cd7d3257
Ivan Duka <ivan.duka.pm@gmail.com>
2020-04-23 14:49:52 -0600

    updating the DB schema

--- Codes/DB_setup_and_CSVs_extraction/DB.sql
+++ Codes/DB_setup_and_CSVs_extraction/DB.sql
@@ -16,6 +16,43 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `blocks`
+--
+
+DROP TABLE IF EXISTS `blocks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `blocks` (
+  `pdfId` int NOT NULL,
+  `page_num` int NOT NULL,
+  `block_order` int NOT NULL,
+  `type` int NOT NULL,
+  `block_width` int DEFAULT NULL,
+  `block_height` int DEFAULT NULL,
+  `bbox_x0` double NOT NULL,
+  `bbox_y0` double NOT NULL,
+  `bbox_x1` double NOT NULL,
+  `bbox_y1` double NOT NULL,
+  `ext` text,
+  `color` int DEFAULT NULL,
+  `xres` int DEFAULT NULL,
+  `yres` int DEFAULT NULL,
+  `bpc` int DEFAULT NULL,
+  `block_area` int DEFAULT NULL,
+  `bbox_width` double NOT NULL,
+  `bbox_height` double NOT NULL,
+  `bbox_area` double NOT NULL,
+  `bbox_area_image` double NOT NULL,
+  `titleTOC` text,
+  `page_name` text,
+  `titleTag` text,
+  `titleFinal` text,
+  PRIMARY KEY (`pdfId`,`page_num`,`block_order`),
+  CONSTRAINT `pdfId_and_page` FOREIGN KEY (`pdfId`, `page_num`) REFERENCES `pages` (`pdfId`, `page_num`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `csvs`
 --
 
@@ -43,7 +80,34 @@ CREATE TABLE `csvs` (
   `hasContent` tinyint DEFAULT NULL,
   PRIMARY KEY (`csvId`),
   UNIQUE KEY `csvId_UNIQUE` (`csvId`),
-  KEY `pdf_idx` (`pdfId`),
+  KEY `pdfs_idx` (`pdfId`),
+  CONSTRAINT `pdfs` FOREIGN KEY (`pdfId`) REFERENCES `pdfs` (`pdfId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `pages`
+--
+
+DROP TABLE IF EXISTS `pages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pages` (
+  `pdfId` int NOT NULL,
+  `page_num` int NOT NULL,
+  `width` int NOT NULL,
+  `height` int NOT NULL,
+  `rotation` int NOT NULL,
+  `figures` int NOT NULL,
+  `num_images` int NOT NULL,
+  `media_x0` int NOT NULL,
+  `media_y0` int NOT NULL,
+  `media_x1` int NOT NULL,
+  `media_y1` int NOT NULL,
+  `media_width` int NOT NULL,
+  `media_height` int NOT NULL,
+  `page_area` int NOT NULL,
+  PRIMARY KEY (`pdfId`,`page_num`),
   CONSTRAINT `pdf` FOREIGN KEY (`pdfId`) REFERENCES `pdfs` (`pdfId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -69,6 +133,27 @@ CREATE TABLE `pdfs` (
   UNIQUE KEY `pdfId_UNIQUE` (`pdfId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `toc`
+--
+
+DROP TABLE IF EXISTS `toc`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `toc` (
+  `assigned_count` int DEFAULT NULL,
+  `title_type` varchar(255) NOT NULL,
+  `titleTOC` text NOT NULL,
+  `page_name` text NOT NULL,
+  `toc_page_num` int NOT NULL,
+  `toc_pdfId` int NOT NULL,
+  `toc_title_order` int NOT NULL,
+  PRIMARY KEY (`toc_page_num`,`toc_pdfId`,`toc_title_order`),
+  KEY `pdf2` (`toc_pdfId`) /*!80000 INVISIBLE */,
+  CONSTRAINT `pdf2` FOREIGN KEY (`toc_pdfId`) REFERENCES `pdfs` (`pdfId`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -79,4 +164,4 @@ CREATE TABLE `pdfs` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-04-15 11:34:00
+-- Dump completed on 2020-04-23 14:46:16
