4fc31d2048c5b245d3b42d6f7abb4e923e931011
Vibudh Singh <Vibudh.Singh@cer-rec.gc.ca>
2020-04-27 12:39:38 -0600

    Metadata functions added

--- Codes/01_Data_Extraction_Preparation/pdf_metadata.py
+++ Codes/01_Data_Extraction_Preparation/pdf_metadata.py
@@ -20,14 +20,52 @@ import pandas as pd
 from pathlib import Path
 
 
+import PyPDF2
+from pathlib import Path
+
+
         
 def check_topic_present(topic, text):
+    """
+    This basic function attempts to check if any of the keywords of a 
+    particular topic are present in the the description line of the PDF File. 
+    
+    Parameters
+    ----------
+    topic: contains a list of keywords present in a topic
+    text: contains the textual description line for the PDF
+    
+    Returns
+    ----------
+    Binary_Value:
+        It returs binary value 1 or 0, depecding on if there is a keyword that 
+        is present in the textual description line for the PDF. 
+        
+    """
+    
     for key in topic:
         if key in text:
             return 1
     return 0
           
 def remove_string_special_characters(s): 
+    """
+    This basic function attempts to replace special characters, and multiple 
+    white spaces with a single pscae. It also removes all leading and trailing 
+    characters from the string. Furthermore, it converts the string into lower 
+    case characters. thus making the input string into easy to process string.
+    
+    Parameters
+    ----------
+    s: the input string 
+    
+    Returns
+    ----------
+    Easy to process string:
+        It reurns easy to process string, which means the string does not have 
+        unnecessary white spaces and all characters are in lower case.
+        
+    """
       
     # removes special characters with ' ' 
     stripped = re.sub('[^a-zA-z\s]', '', s) 
@@ -38,10 +76,41 @@ def remove_string_special_characters(s):
       
     # Remove start and end white spaces 
     stripped = stripped.strip() 
-    if stripped != '': 
-            return stripped.lower() 
+    return stripped.lower() 
         
 def pdf_categorize(path, Index0):
+    """
+    This method attempts to categorize all the PDF files from the list of PDFs 
+    into one more ESA categories. 
+    
+    Based on the contents of a ESA files, we were able to identify the top 10 
+    ESA categories. We performed frequency analysis of the keywords from the 
+    PDF contents, thereafter we came up with top 10 ESA categories. 
+    
+    As there might be a single PDF File covering more than one ESA category, 
+    hence, we get an arror of categories for each PDF File. PDF files which do 
+    not match to any of these 10 categories are classifies as 'other'.
+    
+    Parameters
+    ----------
+    path: path of the root folder in string format
+        This path will be used to find the folder location where the scraped 
+        pdf files are going to be saved
+    Index0: Dataframe with the DataIDs of the PDF and their downloadable links    
+        Data_ID: It is the unique ID of the PDF file which will be used as the 
+        name of the PDF downloaded
+        esa_download_link: URL addresses stored as a list of string
+        a list of the pdf URLs so that the respepctive files could be 
+        downloaded
+        
+    Returns
+    ----------
+    Index1:
+        This returns a dataframe similar to Index0 with an additional coloumn
+        'Topics', indicating the ESA categories which the code has identified 
+        for the PDF file.
+        
+    """
     
     #Defining 10 categories based on the keywords chosen 
     land1 = ['soil', 'land', 'ground', 'terrain', 'topography', 'ecozones', 'terrain']
@@ -110,21 +179,88 @@ def pdf_categorize(path, Index0):
         
     Index1 = Index0
     Index1['Topics'] = topics
-    
     return(Index1)
 
 
 def pdf_size(path, Index0):
+    """
+    This method attempts to identify the size of each of the PDF files (in 
+    bytes) from the list of files present in the Index0. The files which might 
+    have an error in opening or any other dependent step will show a file size 
+    of 0 bytes. 
+    
+    Parameters
+    ----------
+    path: path of the root folder in string format
+        This path will be used to find the folder location where the scraped 
+        pdf files are going to be saved
+    Index0: Dataframe with the DataIDs of the PDF and their downloadable links    
+        Data_ID: It is the unique ID of the PDF file which will be used as the 
+        name of the PDF downloaded
+        esa_download_link: URL addresses stored as a list of string
+        a list of the pdf URLs so that the respepctive files could be 
+        downloaded
+        
+    Returns
+    ----------
+    Index1:
+        This returns a dataframe similar to Index0 with an additional coloumn
+        'PDF Size (bytes)', indicating the size of the PDF file in bytes. 
+        
+    """
     sizes = []
     for index, row in Index0.iterrows():
-        pdf_path = path + "\\Data Files\\PDFs\\" + str(row['Data ID']) + '.pdf'
-        file = Path(pdf_path)
-        size = file.stat().st_size
-        sizes.append(size)
+        try:
+            pdf_path = path + "\\Data_Files\\PDFs\\" + str(row['Data ID']) + '.pdf'
+            file = Path(pdf_path)
+            size = file.stat().st_size
+            sizes.append(size)
+        except:
+            sizes.append(0)
     Index1 = Index0
     Index1['PDF Size (bytes)'] = sizes
     return(Index1)
             
 
-#def pdf_pagenumbers(path, Index0):
+def pdf_pagenumbers(path, Index0):
+    """
+    This method attempts to identify the number of pages in each of the PDF 
+    files from the list of files present in the Index0. The files which might 
+    have an error in opening due to any other dependent step will have 0 number 
+    of pages in the pdf. 
+    
+    Parameters
+    ----------
+    path: path of the root folder in string format
+        This path will be used to find the folder location where the scraped 
+        pdf files are going to be saved
+    Index0: Dataframe with the DataIDs of the PDF and their downloadable links    
+        Data_ID: It is the unique ID of the PDF file which will be used as the 
+        name of the PDF downloaded
+        esa_download_link: URL addresses stored as a list of string
+        a list of the pdf URLs so that the respepctive files could be 
+        downloaded
+        
+    Returns
+    ----------
+    Index1:
+        This returns a dataframe similar to Index0 with an additional coloumn
+        'Number of Pages', indicating the number of pages in the PDF file. 
+        
+    """
+    page_numbers = []
+    for index, row in Index0.iterrows():
+        try:
+            pdf_path = Path(path + "\\Data_Files\\PDFs\\" + str(row['Data ID']) + '.pdf')
+            with pdf_path.open("rb") as pdf:
+                reader = PyPDF2.PdfFileReader(pdf)
+                if reader.isEncrypted:
+                    reader.decrypt("")
+                total_pages = reader.getNumPages()
+                page_numbers.append(total_pages)
+        except:
+            page_numbers.append(0)
+    Index1 = Index0
+    Index1['Number of Pages'] = page_numbers
+    return(Index1)
     
