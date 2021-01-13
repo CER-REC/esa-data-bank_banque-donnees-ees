e6d1c89084a7b1704468b40b7972a69ef4fe6d4d
bwei-cer <bingjie.wei@cer-rec.gc.ca>
2021-01-13 16:58:40 -0700

    clean up

--- Codes/Section_04_Final_Data_Merge_and_Visualization/duplicates.py
+++ Codes/Section_04_Final_Data_Merge_and_Visualization/duplicates.py
@@ -33,8 +33,33 @@ df_merge_dup = df_merge[df_merge['count'] > 1]
 # 8      5
 # 7      5
 # 9      2
+df_meta = df_index.groupby(['Title', 'Data ID', 'PDF Page Number'])[['PDF Download URL', 'Download folder name']].first().reset_index()
 
-df_merge_dup = df_merge_dup.join(df_index[['PDF Download URL', 'Application Name', 'Application Filing Date', 'Consultant Name', 'Download folder name']], how='left')
+df_merge_dup = df_merge_dup.merge(df_meta)
+# df_merge_dup['Download folder name'].value_counts()
+# brnswck              111
+# strnmnln             104
+# wlvrnrvrltrlllp       80
+# lsmrkttlrvrcrssvr     70
+# twrbrch               40
+# tmx                   18
+# nrgst                 13
+# 2021ngtl              12
+# vntg                  10
+# kystnxl                5
+# nrthcrrdr              5
+# kmnrth                 3
+# nrthmntn               3
+# kystn                  2
+# bkkn                   2
+# dsnmnln                2
+# 2017ngtl               2
+# sprcrdg                2
+# wstpthdlvr             2
+# nrthrngtwy             1
+# lbrtclppr              1
+# kwn                    1
+# sthrnlghts             1
+# cshng                  1
 
-df_merge_dup.sort_values(['Application Filing Date', 'PDF Page Number'], ascending=[False, True])\
-    .to_csv('index_duplicates.csv', index=False)
+df_merge_dup.to_csv('index_duplicates.csv', index=False)
