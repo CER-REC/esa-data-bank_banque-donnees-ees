7162082cacc5229f3e1885eae60d429e4d82be65
bwei-cer <bingjie.wei@cer-rec.gc.ca>
2021-01-27 16:15:54 -0700

    updated comments

--- Codes/Section_04_Final_Data_Merge_and_Visualization/translation/esa_section.py
+++ Codes/Section_04_Final_Data_Merge_and_Visualization/translation/esa_section.py
@@ -3,22 +3,19 @@ import pandas as pd
 file = 'F:/Environmental Baseline Data/Version 4 - Final/Indices/English file sent to Translation - for French index file ESA_website_FRA.csv'
 df_translation_eng = pd.read_csv(file, encoding='latin-1')
 
-# file_fra ='F:/Environmental Baseline Data/Version 4 - Final/Indices/Final ESA index file - translated - Jan 8 2021.xlsx'
-# df_translation_fra = pd.read_excel(file_fra, sheet_name='200505-1-Data - Translated')
-
-# We assume that section cells with comma in it are not translated correctly
-# There are section items like below, but we will just separate them by ',':
+# We found that section cells with commas in it are not translated correctly
+# i.e. Section 13.1: Introduction, Section 13.1: Project Description, Section 13.3: Assessment Methods
+# However, there are section items like below, but we will just separate them by ',':
 # Section 9: Wildlife (Volume 6A, Pipeline and Tank Terminal)
 # Section 8: Inspection, Monitoring, and Follow-Up
 # Section 21: Accidents, Malfunctions and Unplanned Events
-df_translation_eng['is_multi_section_item'] = df_translation_eng['ESA Section(s)']\
+df_translation_eng['is_multi_item_section'] = df_translation_eng['ESA Section(s)']\
     .apply(lambda x: len(x.split(',')) > 1 if type(x) == str else False)
 
-sections = df_translation_eng[df_translation_eng['is_multi_section_item']]['ESA Section(s)'].unique().tolist()
-
+multi_item_sections = df_translation_eng[df_translation_eng['is_multi_item_section']]['ESA Section(s)'].unique().tolist()
 
 section_items = set()
-for section in sections:
+for section in multi_item_sections:
     for item in section.split(','):
         if item.strip():
             section_items.add(item.strip())
