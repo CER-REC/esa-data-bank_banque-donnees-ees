8df59d5460666ee97d96f7e2e9946f27ec6d46ea
Jacques Thibodeau <thibo.jacques@gmail.com>
2021-08-23 13:28:07 -0600

    Creating dataframe with csv contents.

--- Codes/Section_05_VEC_Labeling_for_Tables/create_csv_of_all_table_texts.py
+++ Codes/Section_05_VEC_Labeling_for_Tables/create_csv_of_all_table_texts.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import csv
 
 index_df = pd.read_csv('G:/ESA_downloads/phase_2_download_files/final_csv_table_index_with_pdf_metadata.csv')
 
@@ -8,5 +9,5 @@ csv_location = 'G:/ESA_downloads/phase_2_download_files/CSVs/'
 
 for i in range(len(index_df)):
     if i == 0:
-        with open(csv_location + index_df['filename'][i], 'w') as csv_file:
-            print(csv_file.read())
+        csv_df = pd.read_csv(csv_location + index_df['filename'][i])
+        print(csv_df)
