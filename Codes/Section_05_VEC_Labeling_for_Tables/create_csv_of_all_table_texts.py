ae230aaa54b4a507f08dd71119cd1876076a4e36
Jacques Thibodeau <thibo.jacques@gmail.com>
2021-08-23 17:34:34 -0600

    Added remove s tags function.

--- Codes/Section_05_VEC_Labeling_for_Tables/create_csv_of_all_table_texts.py
+++ Codes/Section_05_VEC_Labeling_for_Tables/create_csv_of_all_table_texts.py
@@ -1,13 +1,27 @@
 import pandas as pd
 import csv
+from pathlib import Path
 
 index_df = pd.read_csv('G:/ESA_downloads/phase_2_download_files/final_csv_table_index_with_pdf_metadata.csv')
 
-print(index_df['filename'])
+# print(index_df['filename'])
 
-csv_location = 'G:/ESA_downloads/phase_2_download_files/CSVs/'
+csv_location = str(Path(__file__).parents[2]) + '\\Data_Files\\CSVs\\new_projects\\'
+print(csv_location)
+
+# csv_location = '/Data_Files/CSVs/new_projects/'
+
+def remove_s_tags(text):
+    return text.replace('<s>', '').replace('</s>', '')
 
 for i in range(len(index_df)):
     if i == 0:
-        csv_df = pd.read_csv(csv_location + index_df['filename'][i])
-        print(csv_df)
+        with open(csv_location + index_df['filename'][i], 'r', encoding='utf-8') as csvfile:
+            reader = csv.reader(csvfile)
+            csv_string = ''
+            for row in reader:
+                csv_string += ''.join(row)
+            csv_string = remove_s_tags(csv_string)
+            print(csv_string)
+        # csv_df = pd.read_csv(csv_location + index_df['filename'][i])
+        # print(csv_df)
