2689de2fd16aa81b71acc8c76133ba969a09822d
Jacques Thibodeau <thibo.jacques@gmail.com>
2021-09-21 20:04:20 -0600

    Fixed sentence tokenizer and added pickle dump.

--- Codes/Section_05_VEC_Labeling_for_Tables/tika_text_extractor.py
+++ Codes/Section_05_VEC_Labeling_for_Tables/tika_text_extractor.py
@@ -3,6 +3,7 @@ import multiprocessing as mp
 from os import listdir, path, makedirs
 from pathlib import Path
 import time
+import pickle
 
 import spacy
 import re
@@ -14,6 +15,11 @@ from sqlalchemy import create_engine
 
 
 nlp = spacy.load('en_core_web_sm')
+
+# run script in root directory
+pdfs_path = str(Path('.') / "Data_Files/esa-project-pdfs-subsample/")
+output_path = pdfs_path + "/extracted_texts/"
+pickled_sentences_path = pdfs_path + "/pickle_lists/"
 
 def list_files(path):
     """This function creates a list of strings with the names of all the PDFs in the specified directory."""
@@ -36,9 +42,6 @@ def remove_false_sentences(sents_list):
 #     """This function takes the extracted text and stores it in our MSSQL database."""
 
 
-# run script in root directory
-pdfs_path = str(Path('.') / "Data_Files/esa-project-pdfs-subsample/")
-output_path = pdfs_path + "/extracted_texts/"
 
 if not path.exists(output_path):
     makedirs(output_path)
@@ -47,6 +50,7 @@ def process_file(filename):
     """This function takes a single PDF file and extracts the text from it."""
     in_filename = pdfs_path + '/' + filename
     out_filename = output_path + '/' + filename[:-4] + '.txt'
+    pickle_filename = pickled_sentences_path + '/' + filename[:-4] + '.pkl'
 
     text = parser.from_file(in_filename)
     with open(out_filename, 'w+') as outfile:
@@ -58,6 +62,7 @@ def process_file(filename):
             sents_list.append(sent.text)
         sents_list = remove_false_sentences(sents_list)
         print(sents_list)
+        pickle.dump(sents_list, open(pickle_filename, 'wb'))
 
 if __name__ == '__main__':
     time_start = time.time()
