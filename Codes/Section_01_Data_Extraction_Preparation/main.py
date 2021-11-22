2d7575770645a501cc339706929775a295dee494
JayThibs <thibo.jacques@gmail.com>
2021-11-22 12:27:22 -0700

    Updated filepaths for new repo structure.

--- Codes/Section_01_Data_Extraction_Preparation/main.py
+++ Codes/Section_01_Data_Extraction_Preparation/main.py
@@ -9,10 +9,11 @@ from Codes.Section_01_Data_Extraction_Preparation.file_preparation import downlo
 from Codes.Section_01_Data_Extraction_Preparation.pdf_metadata import get_pdf_metadata
 
 
-Index0_path = os.path.join(os.getcwd(), 'Input_Files/Phase2_Index_of_PDFs_for_Major_Projects_with_ESAs.csv')
+# Update the path when adding a new list of projects
+Index0_path = os.path.join(os.getcwd(), 'data/external/Phase2_Index_of_pdfs_for_Major_Projects_with_ESAs.csv')
 
+# Load the list of projects
 Index0 = pd.read_csv(Index0_path)
-# Index0 = Index0.head(3)
 
 # Download files
 count = download_file(os.getcwd(), Index0)
@@ -23,11 +24,11 @@ count = rotate_pdf(os.getcwd(), Index0)
 print("{} Files were successfully rotated".format(count))
 
 # Convert to pickle files
-for pdf_folder_name, pickle_file_folder_name in {('PDFs', 'Pickle_Files'), ('PDFs_Rotated', 'Pickle_Files_Rotated')}:
-    pdf_folder_path = os.path.join(os.getcwd(), 'Data_Files', pdf_folder_name)
+for pdf_folder_name, pickle_file_folder_name in {('pdfs', 'pickle_files'), ('rotated_pdfs', 'pickle_rotated_files')}:
+    pdf_folder_path = os.path.join(os.getcwd(), 'data', pdf_folder_name)
     pdf_file_paths = [os.path.join(pdf_folder_path, file)
                       for file in os.listdir(pdf_folder_path) if file.endswith('.pdf')]
-    pickle_folder_path = os.path.join(os.getcwd(), 'Data_Files', pickle_file_folder_name)
+    pickle_folder_path = os.path.join(os.getcwd(), 'data', pickle_file_folder_name)
 
     # multiprocessing
     args = [(file, pickle_folder_path) for file in pdf_file_paths]
@@ -40,5 +41,5 @@ for pdf_folder_name, pickle_file_folder_name in {('PDFs', 'Pickle_Files'), ('PDF
 
 # Add metadata and export to csv
 Index1 = get_pdf_metadata(os.getcwd(), Index0)
-metadata_file_path = os.path.join(os.getcwd(), 'Output_Files/Index 1 - PDFs for Major Projects with ESAs.csv')
+metadata_file_path = os.path.join(os.getcwd(), 'data/output/Index 1 - pdfs for Major Projects with ESAs.csv')
 Index1.to_csv(metadata_file_path, index=False, encoding='utf-8-sig')
